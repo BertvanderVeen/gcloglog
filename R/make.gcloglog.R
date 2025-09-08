3dde08cdd1ca2efa06907fac61adb52dd255c100
bertv <bert_van_der_veen@hotmail.com>
2025-09-08 16:50:38 +0200

    uncomment line

--- R/make.gcloglog.R
+++ R/make.gcloglog.R
@@ -41,7 +41,7 @@ make.gcloglog <- function(phi) {
     dev.resids = binomial()$dev.resids,
     aic = binomial()$aic,
     mu.eta = function(eta) {
-      # eta <-  pmin(eta, 700) # borrowed from R's cloglog
+      eta <-  pmin(eta, 700) # borrowed from R's cloglog
       min(phi^(phi+1),9e9)*exp( eta - (phi + 1) * (eta + log1p(phi * exp(-eta))))
     },
     validmu = function(mu)all(is.finite(mu)) && all(mu > 0 & mu < 1),
